6501dd868a33981c0be1dfa167b18dca38c72ca1
tiochoirul <mtio871@gmail.com>
2023-06-19 15:51:42 +0700

    Add first commit

---
+++ SpotifyCoverDownloader/frmDownloader.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace SpotifyCoverDownloader
+{
+    partial class frmDownloader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDownloader));
+            this.lblAlbumUrl = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pbCover = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCover)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblAlbumUrl
+            // 
+            this.lblAlbumUrl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAlbumUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAlbumUrl.ForeColor = System.Drawing.Color.White;
+            this.lblAlbumUrl.Location = new System.Drawing.Point(12, 65);
+            this.lblAlbumUrl.Name = "lblAlbumUrl";
+            this.lblAlbumUrl.Size = new System.Drawing.Size(337, 49);
+            this.lblAlbumUrl.TabIndex = 0;
+            this.lblAlbumUrl.Text = "Click Here for Album Url";
+            this.lblAlbumUrl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblAlbumUrl.Click += new System.EventHandler(this.lblAlbumUrl_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(12, 128);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(337, 39);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pbCover
+            // 
+            this.pbCover.Image = ((System.Drawing.Image)(resources.GetObject("pbCover.Image")));
+            this.pbCover.Location = new System.Drawing.Point(29, 198);
+            this.pbCover.Name = "pbCover";
+            this.pbCover.Size = new System.Drawing.Size(300, 300);
+            this.pbCover.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbCover.TabIndex = 2;
+            this.pbCover.TabStop = false;
+            this.pbCover.Click += new System.EventHandler(this.pbCover_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(337, 46);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Spotify Album Cover Downloader";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(9, 517);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "created by tcm";
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnClose.BackgroundImage")));
+            this.btnClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnClose.Location = new System.Drawing.Point(338, 3);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(20, 20);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmDownloader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.CancelButton = this.btnClose;
+            this.ClientSize = new System.Drawing.Size(361, 539);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbCover);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblAlbumUrl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmDownloader";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Spotify Cover Downloader";
+            this.Load += new System.EventHandler(this.frmDownloader_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCover)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAlbumUrl;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox pbCover;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ SpotifyCoverDownloader/frmDownloader.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Runtime.InteropServices;
+
+namespace SpotifyCoverDownloader
+{
+    public partial class frmDownloader : Form
+    {
+        private HttpClient httpClient;
+
+        public frmDownloader()
+        {
+            InitializeComponent();
+
+            // Create the HttpClient instance with User-Agent header
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36");
+
+        }
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            string spotifyTrackUrl = lblAlbumUrl.Text;
+            string oembedEndpoint = "https://open.spotify.com/oembed?url=" + spotifyTrackUrl;
+
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(oembedEndpoint);
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+
+                // Parse the JSON response
+                JObject json = JObject.Parse(jsonResponse);
+                string picUrl = json["thumbnail_url"].ToString();
+
+                pbCover.Load(picUrl);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error");
+            }
+        }
+
+        private void lblAlbumUrl_Click(object sender, EventArgs e)
+        {
+            lblAlbumUrl.Text = Clipboard.GetText();
+        }
+
+        private void frmDownloader_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbCover_Click(object sender, EventArgs e)
+        {
+            string download = Environment.GetEnvironmentVariable("USERPROFILE") + @"\" + "Downloads" + @"\" + "cover.jpg";
+
+            try
+            {
+                // Check if file exists with its full path
+                if (System.IO.File.Exists(download))
+                {
+                    // If file found, delete it
+                    System.IO.File.Delete(download);
+                }
+            }
+            catch (System.IO.IOException ioExp)
+            {
+                Console.WriteLine(ioExp.Message);
+            }
+
+            pbCover.Image.Save(download, System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public static class Clipboard
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr GetClipboardData(uint uFormat);
+        [DllImport("user32.dll")]
+        static extern bool IsClipboardFormatAvailable(uint format);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool OpenClipboard(IntPtr hWndNewOwner);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool CloseClipboard();
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GlobalLock(IntPtr hMem);
+        [DllImport("kernel32.dll")]
+        static extern bool GlobalUnlock(IntPtr hMem);
+
+        const uint CF_UNICODETEXT = 13;
+
+        public static string GetText()
+        {
+            if (!IsClipboardFormatAvailable(CF_UNICODETEXT))
+                return null;
+            if (!OpenClipboard(IntPtr.Zero))
+                return null;
+
+            string data = null;
+            var hGlobal = GetClipboardData(CF_UNICODETEXT);
+            if (hGlobal != IntPtr.Zero)
+            {
+                var lpwcstr = GlobalLock(hGlobal);
+                if (lpwcstr != IntPtr.Zero)
+                {
+                    data = Marshal.PtrToStringUni(lpwcstr);
+                    GlobalUnlock(lpwcstr);
+                }
+            }
+            CloseClipboard();
+
+            return data;
+        }
+    }
+}
